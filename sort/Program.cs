52272944cb94a561ce237096d24ecc6f7af9a672
VladislavVinyukov <43349653+VladislavVinyukov@users.noreply.github.com>
2019-03-18 10:24:38 +0500

    Optimization

--- sort/Program.cs
+++ sort/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,35 +10,24 @@ namespace sort
     {      
         static void Main(string[] args)
         {
-            string srt = Console.ReadLine();
-            int number = Convert.ToInt32(srt);
-            int[] mass = new int[number];          
-            int a = 2;
-            for (int t = 0; t < mass.Length; t++)
+            while (true)
             {
-                mass[t] = a;
-                a++;
-            }
-            int[] x = new int[number];
-            for (int i = 0; i < mass.Length; i++)
-            {
-                if (mass[i] % 2 == 0)
+                Console.WriteLine("Введите число");
+                string srt = Console.ReadLine();
+                int number = Convert.ToInt32(srt);
+                if (number < 10000 && number > 2)
                 {
-                    Console.Write(mass[i] + " ");
-                }else
-                {
-                    x[i] = mass[i];
+                    int[] mass = new int[number];
+                    int[] x = new int[number];
+                    int a = 1;
+                    for (int t = 0; t < mass.Length; t++) { mass[t] = a; a++; }
+                    for (int i = 0; i < mass.Length; i++) { if (mass[i] % 2 == 0) Console.Write(mass[i] + " "); else x[i] = mass[i]; }
+                    Array.Reverse(x);
+                    for (int h = 0; h < x.Length; h++) if (x[h] != 0) Console.Write(x[h] + " ");
+                    Console.WriteLine();
                 }
-            }
-            Array.Reverse(x);
-            for (int h = 0; h < x.Length; h++)
-            {
-                if(x[h] != 0)
-                {
-                    Console.Write(x[h] + " ");
-                }               
-            }
-            Console.ReadLine();
+                else { Console.Clear(); Console.WriteLine("Введите другое число от 2 до 10 000"); }
+            }                   
         }
     }
 }
